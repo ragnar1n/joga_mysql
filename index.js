e406a09e19afc647fc256af8458a48df5a1efad2
Ragnar <ragnar.naruski@voco.ee>
2023-01-10 23:25:22 +0200

    Created author controller and route, refactored application code

--- index.js
+++ index.js
@@ -10,47 +10,18 @@ app.engine('hbs',hbs.engine({
     defaultLayout:'main',
     layoutsDir:__dirname+'/views/layouts/',
 }))
-
 app.use(express.static('public'))
 
-const mysql = require('mysql');
+const bodyParser=require('body-parser')
+app.use(bodyParser.urlencoded({extended:true}))
 
-const con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "qwerty",
-    database: 'joga_mysql'
-});
+const articleRoutes=require('./routes/article');
+const authorRoutes=require('./routes/author')
 
-con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-});
-
-app.get('/',(req,res)=>{
-    let sql=`SELECT*FROM article;`
-    con.query(sql,(err,result)=>{
-        if (err) throw err;
-        res.render('index',{
-            articles:result
-        })
-    })
-
-})
-
-app.get('/article/:slug',(req,res)=>{
-    let sql=`SELECT*FROM article where slug="${req.params.slug}"`
-    con.query(sql,(err,result)=>{
-        if (err) throw err;
-        res.render('article',{
-            article:result
-        })
-    })
-})
+app.use('/',articleRoutes)
+app.use('/article',articleRoutes)
+app.use('/author',authorRoutes)
 
 app.listen(3004,()=>{
  console.log('Server started')
 })
-
-// app.use(express.json())
-// app.use
